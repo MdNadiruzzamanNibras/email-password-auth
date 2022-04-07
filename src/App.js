86e27094712fd96d796ac98aa553f35186ef541d
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-07 19:42:14 +0600

    create with emai password

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import app from "./firebase.init";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
+import { useState } from "react";
 
 
 const auth = getAuth(app);
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const handleEmailBlur= event=>{
-    console.log(event.target.value)
+   setEmail(event.target.value)
   }
   const handlePasswordBlur= event=>{
-    console.log(event.target.value)
+    setPassword(event.target.value)
   }
   const handleSubmit= event=>{
-    console.log("subimt formed")
-    event.preventDefult()
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((result)=>{
+      const user = result.user
+      console.log(user)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+    event.preventDefault()
   }
   return (
     <div>
