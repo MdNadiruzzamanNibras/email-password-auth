a25f0872d499fcf3195132c8491696d55e10e54b
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-07 19:59:15 +0600

    required

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import { useState } from "react";
 
 
 const auth = getAuth(app);
+
 function App() {
+  const [validated, setValidated] = useState(false);
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const handleEmailBlur= event=>{
@@ -18,6 +20,13 @@ function App() {
     setPassword(event.target.value)
   }
   const handleSubmit= event=>{
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
     createUserWithEmailAndPassword(auth, email, password)
     .then((result)=>{
       const user = result.user
@@ -33,10 +42,13 @@ function App() {
      
       <div className="registration w-50 mx-auto">
       <h2 className="text-primary mt-4">Please Registration</h2>
-      <Form onSubmit={handleSubmit}>
+      <Form noValidate validated={validated}  onSubmit={handleSubmit}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+    <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+    <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
@@ -44,7 +56,10 @@ function App() {
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+    <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+    <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
   </Form.Group>
   <Button variant="primary" type="submit">
     Submit
